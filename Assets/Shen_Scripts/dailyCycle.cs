fd49cf99bcd92f463cace10b4cf945b0594f8843
Shen-2102687 <100612118+Shen-2102687@users.noreply.github.com>
2022-10-04 20:57:29 +0200

    added day colour change and enemy spawn

--- Assets/Shen_Scripts/dailyCycle.cs
+++ Assets/Shen_Scripts/dailyCycle.cs
@@ -16,10 +16,27 @@ public class dailyCycle : MonoBehaviour
     public Text timeText;
     public Text cycleValText;
 
+    //public GameObject cameraObj;
+
+    public Camera cameraObj;
+    Color32 afternoonColour = new Color32(186, 188, 127, 255);
+    Color32 morningColour = new Color32(223, 224, 171, 255);
+    Color32 eveningColour = new Color32(149, 150, 99, 255);
+    Color32 nightColour = new Color32(117, 117, 109, 255);
+
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
+    public Transform spawnPoint3;
+
+    public GameObject enemy;
+    bool instantiated = false;
+
     // Start is called before the first frame update
     void Start()
     {
         angularFrequency = 2 * Mathf.PI * (frequency);
+
+        
     }
 
     // Update is called once per frame
@@ -38,30 +55,55 @@ public class dailyCycle : MonoBehaviour
 
         if ( 0 < cycleFunction && cycleFunction < 1 && cycleFunction < nextCycleVal)
         {
+            instantiated = false;
             //its morning
             cycleText.text = "Morning";
-            
+            cameraObj.backgroundColor = morningColour;
+
+            GameObject[] enemyList = GameObject.FindGameObjectsWithTag("enemy");
+            foreach (GameObject enemy in enemyList)
+            {
+                Destroy(enemy.gameObject);
+            }
+
         }
 
         if (0 < cycleFunction && cycleFunction < 1 && cycleFunction > nextCycleVal)
         {
             //its morning
             cycleText.text = "Afternoon";
+            cameraObj.backgroundColor = afternoonColour;
+
             
+
         }
 
         if (-1 < cycleFunction && cycleFunction < 0 && cycleFunction > nextCycleVal)
         {
             //its morning
             cycleText.text = "Evening";
-            
+            cameraObj.backgroundColor = eveningColour;
         }
 
         if (-1 < cycleFunction && cycleFunction < 0 && cycleFunction < nextCycleVal)
         {
             //its morning
             cycleText.text = "Night";
-            
+            cameraObj.backgroundColor = nightColour;
+
+            //Instantiate(enemy, spawnPoint1.transform.position, Quaternion.identity);
+            //Instantiate(enemy, spawnPoint2.transform.position, Quaternion.identity);
+            //Instantiate(enemy, spawnPoint3.transform.position, Quaternion.identity);
+
+            if (!instantiated)
+            {
+                Instantiate(enemy, spawnPoint1.transform.position, Quaternion.identity);
+                Instantiate(enemy, spawnPoint2.transform.position, Quaternion.identity);
+                Instantiate(enemy, spawnPoint3.transform.position, Quaternion.identity);
+                instantiated = true;
+            }
+
+
         }
 
     }
